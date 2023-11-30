49bf47f893005fe6d7b6cbbf5169c14c9e78cc99
cristian021195 <cristiangramajo015@gmail.com>
2023-11-29 22:06:26 -0300

    corregido talentosa

--- 2-talentoso.pl
+++ 2-talentoso.pl
@@ -10,11 +10,7 @@ talento(ana, escribir).
 
 
 /* i : talentoso - consultar: una persona es talentosa si tiene por lo menos 2 talentos */
-/*aggregate(count, X^fact('A',X), Count).  aggregate_all(count, is_man(X), Count)*/
-incremento(X, R) :- R is X+1.
-
-/*talentosa(X, R) :- R is 0,talento(X, Z), R is incremento(R, B).*/
-talentosa(X, R) :- talento(X, Z), R is 0.
+talentosa(X) :- talento(X, Z), talento(X, Y), Z \= Y.
 
 /* ii : dos personas son semejantes si poseen el mismo talento */
 semejantes(X, Y) :- talento(X, Z), talento(Y,Z).
