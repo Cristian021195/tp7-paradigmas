3cdd67d778b18c8f63f4c53de5050cea58f301ad
cristian021195 <cristiangramajo015@gmail.com>
2023-11-29 22:07:44 -0300

    corregido talentosa

--- 2-talentoso.pl
+++ 2-talentoso.pl
@@ -9,7 +9,7 @@ talento(luisa, escribir).
 talento(ana, escribir).
 
 
-/* i : talentoso - consultar: una persona es talentosa si tiene por lo menos 2 talentos */
+/* i : talentoso, una persona es talentosa si tiene por lo menos 2 talentos */
 talentosa(X) :- talento(X, Z), talento(X, Y), Z \= Y.
 
 /* ii : dos personas son semejantes si poseen el mismo talento */
